92c784d1997589c2df3153e333148c3fb1f02e6e
leecatrina <cninalee3@gmail.com>
2025-10-09 03:24:43 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?: ");
+        String name = scanner.nextLine();
+
+        System.out.println("How many hours have you worked?" );
+        float hours = scanner.nextFloat();
+
+        System.out.println("What is your pay rate? ");
+        float rate = scanner.nextFloat();
+
+        float grossPay = rate * hours;
+
+        System.out.printf("Employee name: %s\nGross pay is: %.2f\n", name, grossPay);
+
+    }
+}
